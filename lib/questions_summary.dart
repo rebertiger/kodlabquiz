b6e7ceff4a84cbb137e9a206a96782f9600e76da
Recep Berke KAPLAN <recepberkekaplan@gmail.com>
2024-07-08 19:22:41 +0300

    Color adjustment on summary screen

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,14 +7,14 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color avatarBackgroundColor = Colors.white10;
-    
     return SizedBox(
       height: 300,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
-            data['chosen_answer'] == data['correct_answer'] ? avatarBackgroundColor=Colors.green : avatarBackgroundColor = Colors.red;
+            Color chosenAnswerColor = data['chosen_answer'] == data['correct_answer']
+                ? const Color.fromARGB(255, 28, 182, 51)
+                : Colors.red.shade500;
             return Container(
               margin: const EdgeInsets.symmetric(vertical: 5),
               padding: const EdgeInsets.all(10),
@@ -26,7 +26,7 @@ class QuestionsSummary extends StatelessWidget {
                 children: [
                   CircleAvatar(
                     radius: 15,
-                    backgroundColor: avatarBackgroundColor,
+                    backgroundColor: chosenAnswerColor,
                     child: Text(
                       ((data['question_index'] as int) + 1).toString(),
                       style: const TextStyle(
@@ -51,16 +51,16 @@ class QuestionsSummary extends StatelessWidget {
                         const SizedBox(height: 5),
                         Text(
                           'Your answer: ${data['chosen_answer'] as String}',
-                          style: const TextStyle(
+                          style: TextStyle(
                             fontSize: 12,
-                            color: Color.fromARGB(255, 183, 98, 198),
+                            color: chosenAnswerColor,
                           ),
                         ),
                         Text(
                           'Correct answer: ${data['correct_answer'] as String}',
                           style: const TextStyle(
                             fontSize: 12,
-                            color: Color.fromARGB(255, 64, 133, 190),
+                            color: Color.fromARGB(255, 28, 182, 51),
                           ),
                         ),
                       ],
